a66bb7538cee225a7790a1c237b2793c9ea116aa
Khosefin <arianshoughi@gmail.com>
2023-12-19 00:10:19 +0330

    add api for each poet pages ...

--- src/app/[poet]/page.tsx
+++ src/app/[poet]/page.tsx
@@ -4,35 +4,87 @@ import Meta from "antd/es/card/Meta";
 import "./poet.css";
 import Image from "next/image";
 import { Button, Card } from "antd";
-
 import { Typography } from "antd";
 import { useState } from "react";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
-const { Paragraph, Text } = Typography;
+export interface poet {
+  htmlText: string;
+  poetOrCat: {
+    poet: {
+      birthPlace: string;
+      birthYearInLHijri: number;
+      deathYearInLHijri: number;
+      deathPlace: string;
+      fullUrl: string;
+      imageUrl: string;
+      name: string;
+      description:string;
+    };
+    cat: {
+      children: [
+        {
+          id: number;
+          title: string;
+          fullUrl: string;
+        }
+      ];
+      poems: [
+        {
+          id: number;
+          title: string;
+          urlSlug: string;
+        }
+      ];
+    };
+  };
+}
 
 export default function Poet({ params }: { params: { poet: string } }) {
-  const [ellipsis, setEllipsis] = useState(true);
+  const [ellipsis] = useState(true);
+  const { Paragraph, Text } = Typography;
+  const { data, isError, isLoading, isSuccess } = useQuery({
+    queryKey: ["Poets"],
+    queryFn: async () => {
+      const data = await axios.get(
+        `https://api.ganjoor.net/api/ganjoor/page?url=%2F${params.poet}&catPoems=true`
+      );
+      return data.data as poet;
+    },
+  });
+
+  console.log(data);
+
   return (
     <div className="tw-flex tw-justify-center tw-gap-5 tw-w-11/12">
-      <div className="tw-drop-shadow-sm tw-cursor-pointer tw-border tw-rounded-xl tw-w-3/5 tw-h-full">
+      <div className="tw-cursor-pointer tw-drop-shadow-md tw-top-20 tw-sticky tw-rounded-xl tw-w-3/5 tw-h-full">
         <Card
           className=" tw-text-justify"
           actions={[
             <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[13px] tw-font-danaSB">
               تولد:
-              <span className="tw-font-danaR tw-text-[13px]">ترکیه</span>
+              <span className="tw-font-danaR tw-text-[13px]">
+                {data?.poetOrCat.poet.birthPlace}
+              </span>
             </p>,
             <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[13px] tw-font-danaSB">
               تاریخ تولد:
-              <span className="tw-font-danaR tw-text-[13px]"> 9999</span>
+              <span className="tw-font-danaR tw-text-[13px]">
+                {data?.poetOrCat.poet.birthYearInLHijri}
+              </span>
             </p>,
             <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[13px] tw-font-danaSB">
               وفات:
-              <span className="tw-font-danaR tw-text-[13px]"> توس</span>
+              <span className="tw-font-danaR tw-text-[13px]">
+                {data?.poetOrCat.poet.deathPlace}
+              </span>
             </p>,
             <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[13px] tw-font-danaSB">
               تاریخ وفات:
-              <span className="tw-font-danaR tw-text-[13px]">9999</span>
+              <span className="tw-font-danaR tw-text-[13px]">
+                {data?.poetOrCat.poet.deathYearInLHijri}
+              </span>
             </p>,
             <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[13px] tw-font-danaSB">
               دوره:
@@ -41,126 +93,35 @@ export default function Poet({ params }: { params: { poet: string } }) {
           ]}
         >
           <Meta
-            className=""
             avatar={
-              <Image
-                src="/gdap.png"
+              <img
+                src={`https://api.ganjoor.net${data?.poetOrCat.poet.imageUrl}`}
                 width="110"
                 height="80"
                 alt="poet picture"
               />
             }
-            title="ابوالقاسم فردوسی"
+            title={data?.poetOrCat.poet.name}
             description={
               <Paragraph
-                className="tw-opacity-70 tw-font-danaL tw-leading-6"
+                className="tw-text-current tw-font-danaL tw-leading-6"
                 ellipsis={
                   ellipsis
                     ? { rows: 3, expandable: true, symbol: "بیشتر" }
                     : false
                 }
-              >
-                مشرف الدین مصلح بن عبدالله شیرازی شاعر و نویسندهٔ بزرگ قرن هفتم
-                هجری قمری است. تخلص او «سعدی» است که از نام اتابک مظفرالدین سعد
-                پسر ابوبکر پسر سعد پسر زنگی گرفته شده است. وی احتمالاً بین
-                سالهای ۶۰۰ تا ۶۱۵ هجری قمری زاده شده است. در جوانی به مدرسهٔ
-                نظامیهٔ بغداد رفت و به تحصیل ادب و تفسیر و فقه و کلام و حکمت
-                پرداخت. سپس به شام و مراکش و حبشه و حجاز سفر کرد و پس از بازگشت
-                به شیراز، به تألیف شاهکارهای خود دست یازید. وی در سال ۶۵۵ ه.ق
-                سعدی‌نامه یا بوستان را به نظم درآورد و در سال بعد (۶۵۶ ه.ق)
-                گلستان را تألیف کرد. علاوه بر اینها قصاید، غزلیات، قطعات، ترجیع
-                بند، رباعیات و مقالات و قصاید عربی نیز دارد که همه را در کلیات
-                وی جمع کرده‌اند. وی بین سالهای ۶۹۰ تا ۶۹۴ هجری قمری در شیراز
-                درگذشت و در همانجا به خاک سپرده شد.
-              </Paragraph>
+              >{data?.poetOrCat.poet.description}</Paragraph>
             }
           />
         </Card>
-        <div>
-          <Button
-            className=" tw-w-full tw-border-red-700 tw-mt-3 tw-text-base tw-h-9"
-            type="primary"
-          >
-            شاهنامه
-          </Button>
-          <Button
-            className="tw-w-full tw-border-red-700 tw-text-red-700 tw-mt-3 tw-text-base tw-h-9"
-            type="default"
-          >
-            هجونامه (منتسب)
-          </Button>
-        </div>
+        <Button
+          className="tw-w-full tw-border-red-700 tw-text-red-700 tw-mt-3 tw-text-base tw-h-9"
+          type="default"
+        >
+          دیوان اشعار
+        </Button>
       </div>
-      <div className="related-images-frame" id="cat-stats">
-        <div className="century"> آمار </div>
-        <div id="stats-section">
-          <p>
-            این آمار از میان ۴۹٬۶۵۸ بیت شعر موجود در گنجور از اشعار این بخش
-            استخراج شده است.
-          </p>
-          <div className="notice">
-            <p>
-              توجه فرمایید که این آمار به دلایلی از قبیل وجود چند نسخه از آثار
-              شعرا در سایت (مثل آثار خیام) و همینطور یک بیت محسوب شدن مصرع‌های
-              بند قالبهای ترکیبی مثل مخمس‌ها تقریبی و حدودی است و افزونگی دارد.
-            </p>
-            <p>
-              آمار همهٔ شعرهای گنجور را <a href="/vazn">اینجا</a> ببینید.
-            </p>
-            <p>
-              وزن‌یابی دستی در بیشتر موارد با ملاحظهٔ تنها یک مصرع از شعر صورت
-              گرفته و امکان وجود اشکال در آن (مخصوصاً اشتباه در تشخیص وزنهای
-              قابل تبدیل از قبیل وزن مثنوی مولوی به جای وزن عروضی سریع مطوی
-              مکشوف) وجود دارد. وزن‌یابی ماشینی نیز که جدیداً با استفاده از
-              امکانات <a href="http://www.sorud.info/">تارنمای سرود</a> اضافه
-              شده بعضاً خطا دارد. برخی از بخشها شامل اشعاری با بیش از یک وزن
-              هستند که در این صورت عمدتاً وزن ابیات آغازین و برای بعضی منظومه‌ها
-              وزن غالب منظومه به عنوان وزن آن بخش منظور شده است.
-            </p>
-          </div>
-          <table>
-            <tr className="h">
-              <td className="c1">ردیف</td> <td className="c2">وزن</td>
-              <td className="c3">تعداد ابیات</td>
-              <td className="c4">درصد از کل</td>
-            </tr>
-            <tr className="e">
-              <td className="c1">۱</td>
-              <td className="c2">
-                <a href="/simi/?v=%D9%81%D8%B9%D9%88%D9%84%D9%86%20%D9%81%D8%B9%D9%88%D9%84%D9%86%20%D9%81%D8%B9%D9%88%D9%84%D9%86%20%D9%81%D8%B9%D9%84%20%28%D9%85%D8%AA%D9%82%D8%A7%D8%B1%D8%A8%20%D9%85%D8%AB%D9%85%D9%86%20%D9%85%D8%AD%D8%B0%D9%88%D9%81%20%DB%8C%D8%A7%20%D9%88%D8%B2%D9%86%20%D8%B4%D8%A7%D9%87%D9%86%D8%A7%D9%85%D9%87%29&a=4&c=32">
-                  فعولن فعولن فعولن فعل (متقارب مثمن محذوف یا وزن شاهنامه)
-                </a>
-              </td>
-              <td className="c3">۴۹٬۶۵۸</td> <td className="c4">۱۰۰٫۰۰</td>
-            </tr>
-          </table>
-          <p>
-            آمار ابیات برچسب‌گذاری شدهٔ این بخش با قالب شعری در گنجور به شرح زیر
-            است:
-          </p>
-          <table>
-            <tr className="h">
-              <td className="c1">ردیف</td> <td className="c2">قالب شعری</td>
-              <td className="c3">تعداد ابیات</td>
-              <td className="c4">درصد از کل</td>
-            </tr>
-            <tr className="e">
-              <td className="c1">۱</td>
-              <td className="c2">
-                <a href="/simi/?f=3&a=4&c=32">مثنوی</a>
-              </td>
-              <td className="c3">۴۹٬۶۵۷</td> <td className="c4">۱۰۰٫۰۰</td>
-            </tr>
-            <tr>
-              <td className="c1">۲</td>
-              <td className="c2">
-                <a href="/simi/?f=7&a=4&c=32">غزل/قصیده/قطعه</a>
-              </td>
-              <td className="c3">۱</td> <td className="c4">۰٫۰۰</td>
-            </tr>
-          </table>
-        </div>
-      </div>
+      {/* <div className="main">{isSuccess && JSON.parse(data.htmlText)}</div> */}
     </div>
   );
 }
--- src/ui/components/poet.tsx
+++ src/ui/components/poet.tsx
@@ -1,71 +1,58 @@
 "use client";
-import { Card, Tag } from "antd";
+import { poet } from "@/app/page";
+import { Card } from "antd";
 import Meta from "antd/es/card/Meta";
-import Image from "next/image";
-
-interface params {
-  img: string;
-  name: string;
-  birthlocation: string;
-  deathlocation: string;
-  century: number;
-}
+import { useRouter } from "next/navigation";
 
 export default function Poets({
-  img,
+  imageUrl,
   name,
-  birthlocation,
-  deathlocation,
-  century,
-}: params) {
+  birthPlace,
+  fullUrl,
+  deathPlace,
+  birthYearInLHijri,
+}: poet) {
+  const router = useRouter();
   return (
-    <>
-    <div className="hover:tw-shadow-xl  tw-drop-shadow-sm tw-cursor-pointer tw-border tw-rounded-xl max-md:tw-hidden">
-      <Card
-        actions={[
-          <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[10px] md:tw-text-[12px] tw-font-danaSB">  تولد:
-            <span className="tw-font-danaR tw-text-[10px] md:tw-text-[12px]"> {birthlocation}</span>
-          </p>,
-          <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[10px] md:tw-text-[12px] tw-font-danaSB">  وفات:
-            <span className="tw-font-danaR tw-text-[10px] md:tw-text-[12px]"> {deathlocation}</span>
-          </p>,
-          <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[10px] md:tw-text-[12px] tw-font-danaSB">  قرن:
-            <span className="tw-font-danaR tw-text-[10px] md:tw-text-[12px]"> {century}</span>
-          </p>,
-       
-        ]}
-      >
-        <Meta
-          className="tw-flex tw-flex-col tw-items-center max-sm:tw-h-14"
-          avatar={<img src={img} className="-tw-translate-x-2 tw-w-14 md:tw-w-20 tw-mb-2 max-sm:tw-absolute max-sm:tw-translate-x-5 max-sm:-tw-translate-y-3" alt="poet picture" />}
-          title={<p className="tw-text-base md:tw-text-lg tw-font-danaL max-sm:tw-absolute max-sm:tw-translate-x-6 max-sm:tw-translate-y-12">{name}</p>}
-        />
-      </Card>
-    </div>
-    <div className="hover:tw-shadow-xl tw-drop-shadow-sm tw-cursor-pointer tw-border tw-rounded-xl md:tw-hidden">
-      <Card
-
-        actions={[
-          <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[13px] tw-font-danaSB">  تولد:
-            <span className="tw-font-danaR tw-text-[13px]"> {birthlocation}</span>
-          </p>,
-          <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[13px] tw-font-danaSB">  وفات:
-            <span className="tw-font-danaR tw-text-[13px]"> {deathlocation}</span>
-          </p>,
-          <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[13px] tw-font-danaSB">  قرن:
-            <span className="tw-font-danaR tw-text-[13px]"> {century}</span>
-          </p>,
-
-        ]}
-      >
-        <Meta
-          className="tw-flex tw-items-center"
-          avatar={<Image src={img} width="70" height="70" alt="poet picture" />}
-          title={name}
-          description="شاعر قرن پنجم"
-        />
-      </Card>
-    </div>
-    </>
+    <Card
+      className="hover:tw-shadow-xl tw-drop-shadow-smtw-cursor-pointer tw-border tw-rounded-xl tw-flex tw-flex-col tw-justify-between "
+      onClick={() => router.push(fullUrl)}
+      actions={[
+        <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[10px] md:tw-text-[12px]  max-md:tw-text-[13px] tw-font-danaSB tw-h-full">
+          تولد:
+          <span className="tw-font-danaR tw-text-[10px] md:tw-text-[12px] max-md:tw-text-[13px]">
+            {birthPlace ? birthPlace : "نامشخص"}
+          </span>
+        </p>,
+        <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[10px] md:tw-text-[12px]  max-md:tw-text-[13px] tw-font-danaSB">
+          وفات:
+          <span className="tw-font-danaR tw-text-[10px] md:tw-text-[12px] max-md:tw-text-[13px]">
+            {deathPlace ? deathPlace : "نامشخص"}
+          </span>
+        </p>,
+        <p className="tw-flex tw-flex-col tw-gap-1 tw-text-[10px] md:tw-text-[12px]  max-md:tw-text-[13px] tw-font-danaSB">
+          قرن:
+          <span className="tw-font-danaR tw-text-[10px] md:tw-text-[12px] max-md:tw-text-[13px]">
+            {birthYearInLHijri}
+          </span>
+        </p>,
+      ]}
+    >
+      <Meta
+        className="tw-flex md:tw-flex-col tw-items-center max-sm:tw-h-14"
+        avatar={
+          <img
+            src={`https://api.ganjoor.net${imageUrl}`}
+            className="sm:-tw-translate-x-2 tw-w-14 md:tw-w-20 tw-mb-2"
+            alt="poet picture"
+          />
+        }
+        title={
+          <p className="tw-text-base md:tw-text-lg tw-font-danaL tw-whitespace-pre-wrap tw-text-center">
+            {name}
+          </p>
+        }
+      />
+    </Card>
   );
 }
