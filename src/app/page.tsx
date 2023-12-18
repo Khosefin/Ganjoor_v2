a003ba08aa1fd135ad216f2f9edfc7fb222c0e71
Khosefin <arianshoughi@gmail.com>
2023-12-19 00:09:15 +0330

    add api to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,38 @@
+"use client";
 import Poet from "@/ui/components/poet";
 import Sidebar from "@/ui/components/sidebar";
+import { useQuery } from "@tanstack/react-query";
+import { Skeleton, Spin } from "antd";
+import axios from "axios";
 
-export default function Home() {
-  const Poets = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-  ];
+export interface poet {
+  id: number;
+  birthPlace: string;
+  birthYearInLHijri: number;
+  deathPlace: string;
+  fullUrl: string;
+  imageUrl: string;
+  name: string;
+}
 
+const Home = () => {
+  const { data, isError, isLoading, isSuccess } = useQuery({
+    queryKey: ["Poets"],
+    queryFn: async () => {
+      const data = await axios.get("https://api.ganjoor.net/api/ganjoor/poets");
+      return data.data;
+    },
+  });
   return (
     <div className="tw-flex tw-flex-col tw-w-11/12 max-md:tw-w-[96%]">
       <div className="tw-flex max-md:tw-flex-col tw-gap-4 max-sm:tw-gap-3">
         <Sidebar />
         <div className="tw-grid tw-grid-cols-2 lg:tw-grid-cols-4 xl:tw-grid-cols-5 max-sm:tw-grid-cols-1 tw-gap-4 tw-w-full">
-          {Poets.map((item) => (
-            <Poet
-              img="/azar.png"
-              name="فردوسی"
-              birthlocation="اصفهان"
-              deathlocation="قم"
-              century={10}
-            />
-          ))}
+          {isSuccess &&
+            data.map((poet: poet) => <Poet key={poet.id} {...poet} />)}
         </div>
       </div>
     </div>
   );
-}
+};
+export default Home;
