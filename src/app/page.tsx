cbc72ad183ead2f216cf56940e4f6a9cf401d1b3
Khosefin <arianshoughi@gmail.com>
2023-12-20 22:36:16 +0330

    css bug fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import Poet from "@/ui/components/poet";
+import PoetList from "@/ui/components/poetList";
 import Sidebar from "@/ui/components/sidebar";
+import Loading from "@/ui/components/loading";
 
 export interface poet {
   id: number;
@@ -29,24 +30,26 @@ const Home = () => {
     },
   });
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   if (isError) {
     return <div>Error fetching poets</div>;
   }
 
   return (
-    <div className="tw-flex tw-flex-col tw-w-11/12 max-md:tw-w-[96%]">
-      <div className="tw-flex max-md:tw-flex-col tw-gap-4 max-sm:tw-gap-3">
-        <Sidebar />
-        <div className="tw-grid tw-grid-cols-2 lg:tw-grid-cols-4 xl:tw-grid-cols-5 max-sm:tw-grid-cols-1 tw-gap-4 tw-w-full">
-          {isSuccess &&
-            data.map((poet: poet) => <Poet key={poet.id} {...poet} />)}
+    <>
+      {isLoading && <Loading />}
+      {isSuccess && (
+        <div className="tw-flex tw-flex-col tw-w-11/12 max-md:tw-w-[96%]">
+          <div className="tw-flex max-md:tw-flex-col tw-gap-4 max-sm:tw-gap-3">
+            <Sidebar />
+            <div className="tw-grid tw-grid-cols-2 lg:tw-grid-cols-4 xl:tw-grid-cols-5 max-sm:tw-grid-cols-1 tw-gap-4 tw-w-full">
+              {data.map((poet: poet) => (
+                <PoetList key={poet.id} {...poet} />
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 export default Home;
